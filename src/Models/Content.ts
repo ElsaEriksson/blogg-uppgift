95c556646f6334f6a220b38ab132911e430ccfb8
Elsa Eriksson <elsa.95.eriksson@gmail.com>
2023-11-23 13:12:24 +0100

    first ts

---
+++ src/Models/Content.ts
@@ -0,0 +1,7 @@
+export class Content {
+    constructor(
+      public title: string,
+      public text: string,
+      public catergory: string,
+    ) {}
+  }
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
 import "./../scss/style.scss";
+import { Content } from "./Models/Content";
+
+
+
+const titleInput = document.getElementById("modal-input-title") as HTMLInputElement;
+const titleFromUser = titleInput.value;
+
+const textInput = document.getElementById("modal-input-text") as HTMLInputElement;
+const textFromUser = textInput.value;
+
+const categoryInput = document.getElementById("SortButton") as HTMLSelectElement;
+const categoryFromUser = categoryInput.value;
+
+const bloggPosted = document.createElement("div");
+bloggPosted.className = "card";
+bloggPosted.innerHTML = `<h2>${titleFromUser}</h2><p>${textFromUser}</p><h4>${categoryFromUser}</h4>` ;
+
+const blogContent = document.getElementById("BloggContent");
+const createBlogPost = document.getElementById("createBlogPost");
+createBlogPost?.addEventListener("click", ()=>{
+blogContent?.appendChild(bloggPosted);
+});
+
