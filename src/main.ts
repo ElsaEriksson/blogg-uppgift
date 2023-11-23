8a1d8a953abce3f5ed7d04481594dd8079220506
Elsa Eriksson <elsa.95.eriksson@gmail.com>
2023-11-23 17:05:59 +0100

    loop

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
 import "./../scss/style.scss";
 import { Content } from "./Models/Content";
 
-const createBlogPostButton = document.getElementById("CreateBlogPost");
-const blogContent = document.getElementById("BloggContent");
+const createBlogPostButton = document.getElementById("CreateBlogPost") as HTMLButtonElement;
+const blogContent = document.getElementById("BloggContent") as HTMLDivElement;
+
+let blogPosts:Content[] = []
+console.log(blogPosts);
+
+function createHTML () {
+    blogContent.innerHTML="";
+    for (let index = 0; index < blogPosts.length; index++) {
+        const newContent = blogPosts[index];
+
+        const blogPostElement = document.createElement("div");
+        blogPostElement.id ="cardPost"
+        blogPostElement.className = "card";
+        blogPostElement.innerHTML = `<h5>${newContent.title}</h5><p>${newContent.text}</p><h7 id="categoryText">${newContent.category}</h7><a href="#" id="goSomewhereButton" class="btn btn-primary"
+        >Go somewhere</a`;
+      
+        
+        blogContent?.appendChild(blogPostElement);
+    }
+}
 
 createBlogPostButton?.addEventListener("click", () => {
+  createBlogPostButton.type = "submit";
   const titleInput = document.getElementById("modal-input-title") as HTMLInputElement;
   const textInput = document.getElementById("modal-input-text") as HTMLInputElement;
   const categoryInput = document.getElementById("Category") as HTMLSelectElement;
@@ -12,21 +32,22 @@ createBlogPostButton?.addEventListener("click", () => {
   
   const titleFromUser = titleInput.value;
   const textFromUser = textInput.value;
-  const categoryFromUser = categoryInput.value;
+  const categoryFromUser = categoryInput.value; 
 
-  
   const newContent = new Content(titleFromUser, textFromUser, categoryFromUser);
-    console.log(newContent);
+  blogPosts.push(newContent);
+  console.log(newContent);
 
-    
-    
+  createHTML ();
  
-  const blogPostElement = document.createElement("div");
-  blogPostElement.id ="cardPost"
-  blogPostElement.className = "card";
-  blogPostElement.innerHTML = `<h5>${newContent.title}</h5><p>${newContent.text}</p><h7 id="categoryText">${newContent.category}</h7><a href="#" id="goSomewhereButton" class="btn btn-primary"
-  >Go somewhere</a`;
+//   const blogPostElement = document.createElement("div");
+//   blogPostElement.id ="cardPost"
+//   blogPostElement.className = "card";
+//   blogPostElement.innerHTML = `<h5>${newContent.title}</h5><p>${newContent.text}</p><h7 id="categoryText">${newContent.category}</h7><a href="#" id="goSomewhereButton" class="btn btn-primary"
+//   >Go somewhere</a`;
 
   
-  blogContent?.appendChild(blogPostElement);
+//   blogContent?.appendChild(blogPostElement);
+
 });
+
